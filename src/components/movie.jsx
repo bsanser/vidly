ecb7d750aa2ef712146dc16b8ddee01c53ea83a0
bsanser <bsanser@gmail.com>
2018-10-09 11:09:57 +0200

    feat: completed table of movies

--- src/components/movie.jsx
+++ src/components/movie.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 
 const Movie = (props) => {
-  const {title, genre, numberInStock, dailyRentalDate } = props.movie;
+  const {_id, title, genre, numberInStock, dailyRentalRate } = props.movie;
   return (
     <tr>
       <th>{title}</th>
       <td>{genre.name}</td>
       <td>{numberInStock}</td>
-      <td>{dailyRentalDate}</td>
+      <td>{dailyRentalRate}</td>
+      <td><button className="btn btn-danger" onClick={() => props.handleDelete(_id)}>Delete Movie</button></td>
    </tr>
    );
 }
--- src/components/moviesList.jsx
+++ src/components/moviesList.jsx
@@ -8,22 +8,34 @@ class MoviesList extends Component {
   state = {
     movies: getMovies()
   }
+
+  handleDelete = movieId => {
+    const movies = [...this.state.movies];
+    const remainingMovies = movies.filter(m => m._id !== movieId);
+    this.setState({movies: remainingMovies});
+  }
+
+  renderMessage() {
+    if (this.state.movies.length === 0 ) return 'No movies in the database';
+    return `Showing ${this.state.movies.length} in the database`;
+
+  }
   
   render() {
     return (
       <React.Fragment>
-        <p>Showing {this.state.movies.length} in the database</p>
-        <table className="table">
-          <thead className="thead-dark">
+        <p className="text-center">{this.renderMessage()}</p>
+        <table className="table m-2">
+          <thead>
             <tr>
-              <th scope="col">Title</th>
-              <th scope="col">Genre</th>
-              <th scope="col">Stock</th>
-              <th scope="col">Rate</th>
+              <th className="border-top-0"scope="col">Title</th>
+              <th className="border-top-0"scope="col">Genre</th>
+              <th className="border-top-0"scope="col">Stock</th>
+              <th className="border-top-0"scope="col">Rate</th>
             </tr>
           </thead>
           <tbody>
-            {this.state.movies.map(m => <Movie key= {m._id} movie={m}/>)}
+            {this.state.movies.map(m => <Movie key= {m._id} movie={m} handleDelete={this.handleDelete} />)}
           </tbody>
         </table>
       </React.Fragment>
