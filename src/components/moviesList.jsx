ff73529e0a85491f3039bd15c24f867948d0f7ba
bsanser <bsanser@gmail.com>
2018-10-09 18:37:24 +0200

    fixed some unnecessary punctuation signs

--- src/components/moviesList.jsx
+++ src/components/moviesList.jsx
@@ -5,10 +5,10 @@ import Movie from '../components/movie';
 const MoviesList = props => {
     const {length: count} = props.movies;
 
-    if (count === 0) return <p className="text-center m-2">No movies in the database</p>;
+    if (count === 0) return <p className="text-center m-3">No movies in the database</p>;
     return (
       <React.Fragment>
-        <p className="text-center m-2">Showing {props.movies.length} in the database</p>;
+        <p className="text-center m-2">Showing {props.movies.length} in the database</p>
       
         <table className="table m-2">
           <thead>
